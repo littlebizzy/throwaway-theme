0789985fdf2bcf7ed3ce8a46b1cd3ccf45e5a6a3
Jesse Nickles <jesse@littlebizzy.com>
2019-10-10 03:11:49 +0700

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,28 +1,49 @@
 <?php
 // Ref: https://training.ithemes.com/webinar/hardcoding-the-wordpress-setup-process/
+// Ref: https://codex.wordpress.org/Option_Reference
 
 // set the options to change
 $option = array(
-    // we don't want no description
+    'admin_email'                   => '',
+    'avatar_rating'                 => 'PG',
+    'avatar_default'                => 'mystery',
     'blogdescription'               => '',
-    // change category base
+    'blogname'                      => '',
     'category_base'                 => '/category',
-    // change tag base
-    'tag_base'                      => '/tag',
-    // disable comments
-    'default_comment_status'        => 'closed',
-    // disable trackbacks
-    'use_trackback'                 => '',
-    // disable pingbacks
+    'close_comments_for_old_posts'  => '0',
+    'close_comments_days_old'       => '90',
+    'comment_max_links'             => '2',
+    'comment_moderation'            => '1',
+    'comments_notify'               => '1',
+    'comment_order'                 => 'asc',
+    'comment_registration'          => '0',
+    'comment_whitelist'             => '0',
+    'comments_per_page'             => '50',
+    'date_format'                   => 'M j, Y',
+    'default_comments_page'         => 'newest',
+    'default_comment_status'        => 'open',
     'default_ping_status'           => 'closed',
-    // disable pinging
-    'default_pingback_flag'         => '',
-    // change the permalink structure
+    'default_pingback_flag'         => '0',
+    'default_role'                  => 'subscriber',
+    'gmt_offset'                    => 'GMT',
+    'home'                          => '',
+    'moderation_notify'             => '1',
+    'page_comments'                 => '0',
     'permalink_structure'           => '/blog/%postname%/',
-    // dont use year/month folders for uploads 
+    'require_name_email'            => '1',
+    'show_avatars'                  => '1',
+    'show_comments_cookies_opt_in'  => '0',
+    'siteurl'                       => '',
+    'start_of_week'                 => '1',
+    'tag_base'                      => '/tag',
+    'thread_comments'               => '0',
+    'thread_comments_depth'         => '2',
+    'time_format'                   => 'g:ia',
+    'timezone_string'               => '',
     'uploads_use_yearmonth_folders' => '1',
-    // don't use those ugly smilies
     'use_smilies'                   => ''
+    'use_trackback'                 => '',
+    'users_can_register'            => '0',
 );
  
 // update wp_options table
